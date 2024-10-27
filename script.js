5ef8e3b9c3da49be8cc24530a5900520032ff65b
VitoriaBarr <vitoria.barrocas2.vmdsb@gmail.com>
2024-10-26 23:27:37 -0300

    Retirando caracteres

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ document.getElementById("contact").addEventListener("input", function () {
 });
 
 const infoField = document.getElementById("additional-info");
-const maxChars = 300;
+const maxChars = 4;
 
 infoField.addEventListener("input", () => {
   if (infoField.value.length > maxChars) {
